e252c3e8dcfbeddd93367c21e06b52ad05f880d0
manny <manny.acquah@gmail.com>
2017-07-13 12:48:17 +0200

    crop + resize

--- image.sh
+++ image.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 filename=./images.csv
 download_dir=./downloads
+width=800
+height=800
 
 IFS=','
 while read line || [[ -n "$line" ]]; do
@@ -14,7 +16,13 @@ while read line || [[ -n "$line" ]]; do
 #    wget ${line} -o ${download_file_name}  || rm -f ${download_file_name}
 #    curl -o ${download_file_name} ${line}
 #    mogrify -resize 800x800 ${resize_file_name} ${download_file_name}
-    convert ${download_file_name} -resize 800x800! ${resize_file_name}
+#    convert ${download_file_name} -resize 800x800! ${resize_file_name}
+    convert ${download_file_name} \
+        -resize "${width}x${height}^" \
+        -gravity Center \
+        -extent "${width}x${height}" \
+        ${resize_file_name}
+
 done < "$filename"
 
 
